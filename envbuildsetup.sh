8dbcf3e7ab97d247192629e5bc6321ebd04d6bb1
rsoft3d <rsoft3d@gmail.com>
2020-05-06 15:26:51 +0700

    Update envbuildsetup.sh

--- envbuildsetup.sh
+++ envbuildsetup.sh
@@ -1,3 +1,3 @@
- export CROSS_COMPILE=/media/rsoft3d/datalinux/kernel/UBERTC-aarch64-linux-android-4.9-kernel-3f2327eeb9b3/bin/aarch64-linux-android- && 
+ export CROSS_COMPILE=/PATHTOTOOLCHAIN/bin/aarch64-linux-android- && 
 export ARCH=arm64 && export SUBARCH=arm64
 
