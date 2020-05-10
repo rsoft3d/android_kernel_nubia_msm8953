7de274c6611cb5e6fb5ea3944b27a8cc434b87ce
rsoft3d <rsoft3d@gmail.com>
2020-05-10 12:56:29 +0700

    fs: sync: Asynchronous Fsync from HTC
    taken from Faux's S5 repo and modified/fixed-up
          for Linux 3.10 and shamu
    
    Change-Id: Ia1917f3def4be0c39fa867a047fa8952b7fd0f42
    Conflicts:
    	drivers/mmc/core/mmc.c
    
    Original commit: https://bitbucket.org/arter97/android_kernel_samsung_exynos7420/commits/8125c532b02a7d8c36d39f18e4ffcce2180e26cc
    
    Conflicts:
    	fs/sync.c
    	include/linux/fs.h

--- fs/sync.c
+++ fs/sync.c
@@ -7,7 +7,6 @@
 #include <linux/fs.h>
 #include <linux/slab.h>
 #include <linux/export.h>
-#include <linux/module.h>
 #include <linux/namei.h>
 #include <linux/sched.h>
 #include <linux/writeback.h>
@@ -17,12 +16,21 @@
 #include <linux/quotaops.h>
 #include <linux/backing-dev.h>
 #include "internal.h"
-
-bool fsync_enabled = true;
-module_param(fsync_enabled, bool, 0755);
+#ifdef CONFIG_ASYNC_FSYNC
+#include <linux/statfs.h>
+#endif
 
 #define VALID_FLAGS (SYNC_FILE_RANGE_WAIT_BEFORE|SYNC_FILE_RANGE_WRITE| \
 			SYNC_FILE_RANGE_WAIT_AFTER)
+#ifdef CONFIG_ASYNC_FSYNC
+#define FLAG_ASYNC_FSYNC        0x1
+static struct workqueue_struct *fsync_workqueue = NULL;
+struct fsync_work {
+	struct work_struct work;
+	char pathname[256];
+};
+#endif
+
 
 /*
  * Do the filesystem syncing work. For simple filesystems
@@ -156,9 +164,6 @@ SYSCALL_DEFINE1(syncfs, int, fd)
 	struct super_block *sb;
 	int ret;
 
-	if (!fsync_enabled)
-		return 0;
-
 	if (!f.file)
 		return -EBADF;
 	sb = f.file->f_dentry->d_sb;
@@ -186,9 +191,6 @@ int vfs_fsync_range(struct file *file, loff_t start, loff_t end, int datasync)
 {
 	struct inode *inode = file->f_mapping->host;
 
-	if (!fsync_enabled)
-		return 0;
-
 	if (!file->f_op->fsync)
 		return -EINVAL;
 	if (!datasync && (inode->i_state & I_DIRTY_TIME)) {
@@ -211,42 +213,129 @@ EXPORT_SYMBOL(vfs_fsync_range);
  */
 int vfs_fsync(struct file *file, int datasync)
 {
-	if (!fsync_enabled)
-		return 0;
-		
 	return vfs_fsync_range(file, 0, LLONG_MAX, datasync);
 }
 EXPORT_SYMBOL(vfs_fsync);
+#ifdef CONFIG_ASYNC_FSYNC
+extern int emmc_perf_degr(void);
+#define LOW_STORAGE_THRESHOLD   786432
+int async_fsync(struct file *file, int fd)
+{
+        struct inode *inode = file->f_mapping->host;
+        struct super_block *sb = inode->i_sb;
+        struct kstatfs st;
+
+        if ((sb->fsync_flags & FLAG_ASYNC_FSYNC) == 0)
+                return 0;
+
+        if (!emmc_perf_degr())
+                return 0;
+
+        if (fd_statfs(fd, &st))
+                return 0;
+
+        if (st.f_bfree > LOW_STORAGE_THRESHOLD)
+                return 0;
+
+        return 1;
+}
+
+static int do_async_fsync(char *pathname)
+{
+        struct file *file;
+        int ret;
+        file = filp_open(pathname, O_RDWR, 0);
+        if (IS_ERR(file)) {
+                pr_debug("%s: can't open %s\n", __func__, pathname);
+                return -EBADF;
+        }
+        ret = vfs_fsync(file, 0);
+
+        filp_close(file, NULL);
+        return ret;
+}
+
+static void do_afsync_work(struct work_struct *work)
+{
+        struct fsync_work *fwork =
+                container_of(work, struct fsync_work, work);
+        int ret = -EBADF;
+
+        pr_debug("afsync: %s\n", fwork->pathname);
+        ret = do_async_fsync(fwork->pathname);
+        if (ret != 0 && ret != -EBADF)
+                pr_info("afsync return %d\n", ret);
+        else
+                pr_debug("afsync: %s done\n", fwork->pathname);
+        kfree(fwork);
+}
+#endif
 
 static int do_fsync(unsigned int fd, int datasync)
 {
+
 	struct fd f = fdget(fd);
 	int ret = -EBADF;
-	
-	if (!fsync_enabled)
-		return 0;
+#ifdef CONFIG_ASYNC_FSYNC
+        struct fsync_work *fwork;
+#endif
 
 	if (f.file) {
+#ifdef CONFIG_ASYNC_FSYNC
+                ktime_t fsync_t, fsync_diff;
+                char pathname[256], *path;
+                path = d_path(&(f.file->f_path), pathname, sizeof(pathname));
+                if (IS_ERR(path))
+                        path = "(unknown)";
+                else if (async_fsync(f.file, fd)) {
+                        if (!fsync_workqueue)
+                                fsync_workqueue =
+                                        create_singlethread_workqueue("fsync");
+                        if (!fsync_workqueue)
+                                goto no_async;
+
+                        if (IS_ERR(path))
+                                goto no_async;
+
+                        fwork = kmalloc(sizeof(*fwork), GFP_KERNEL);
+                        if (fwork) {
+                                strncpy(fwork->pathname, path,
+                                        sizeof(fwork->pathname) - 1);
+                                INIT_WORK(&fwork->work, do_afsync_work);
+                                queue_work(fsync_workqueue, &fwork->work);
+                                fdput(f);
+                                return 0;
+                        }
+                }
+no_async:
+                fsync_t = ktime_get();
+#endif
+
 		ret = vfs_fsync(f.file, datasync);
 		fdput(f);
 		inc_syscfs(current);
+#ifdef CONFIG_ASYNC_FSYNC
+                fsync_diff = ktime_sub(ktime_get(), fsync_t);
+                if (ktime_to_ms(fsync_diff) >= 5000) {
+                        pr_info("VFS: %s pid:%d(%s)(parent:%d/%s)\
+                                takes %lld ms to fsync %s.\n", __func__,
+                                current->pid, current->comm,
+                                current->parent->pid, current->parent->comm,
+                                ktime_to_ms(fsync_diff), path);
+                }
+#endif
+
 	}
 	return ret;
 }
 
 SYSCALL_DEFINE1(fsync, unsigned int, fd)
 {
-	if (!fsync_enabled)
-		return 0;
-
 	return do_fsync(fd, 0);
 }
 
 SYSCALL_DEFINE1(fdatasync, unsigned int, fd)
 {
-	if (!fsync_enabled)
-		return 0;
-		
 	return do_fsync(fd, 1);
 }
 
@@ -305,9 +394,6 @@ SYSCALL_DEFINE4(sync_file_range, int, fd, loff_t, offset, loff_t, nbytes,
 	struct address_space *mapping;
 	loff_t endbyte;			/* inclusive */
 	umode_t i_mode;
-
-	if (!fsync_enabled)
-		return 0;
 
 	ret = -EINVAL;
 	if (flags & ~VALID_FLAGS)
@@ -369,8 +455,7 @@ SYSCALL_DEFINE4(sync_file_range, int, fd, loff_t, offset, loff_t, nbytes,
 	}
 
 	if (flags & SYNC_FILE_RANGE_WRITE) {
-		ret = __filemap_fdatawrite_range(mapping, offset, endbyte,
-						 WB_SYNC_NONE);
+		ret = filemap_fdatawrite_range(mapping, offset, endbyte);
 		if (ret < 0)
 			goto out_put;
 	}
