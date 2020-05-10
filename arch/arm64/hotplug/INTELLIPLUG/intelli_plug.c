1ae9a7c88c938619d566b7d758fedcae33c0086e
rsoft3d <rsoft3d@gmail.com>
2020-05-10 19:11:52 +0700

    FIX INTELLIPLUG
    @2020 Modified HOTPLUG by  Abdur Rofik,( rsoft3d@gmail.com) Fix for lineage OS 16.0 and kernel 3.18

--- arch/arm64/hotplug/INTELLIPLUG/intelli_plug.c
+++ arch/arm64/hotplug/INTELLIPLUG/intelli_plug.c
@@ -11,7 +11,7 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+ *@2020 Modified by  Abdur Rofik,( rsoft3d@gmail.com) Fix for lineage OS 16.0
  */
 #include <linux/workqueue.h>
 #include <linux/cpu.h>
@@ -163,7 +163,8 @@ extern unsigned long avg_cpu_nr_running(unsigned int cpu);
 
 static unsigned int calculate_thread_stats(void)
 {
-	unsigned int avg_nr_run = avg_nr_running();
+
+	//unsigned int avg_nr_run = avg_nr_running();
 	unsigned int nr_run;
 	unsigned int threshold_size;
 	unsigned int *current_profile;
@@ -191,7 +192,7 @@ static unsigned int calculate_thread_stats(void)
 
 		if (nr_run_last <= nr_run)
 			nr_threshold += nr_run_hysteresis;
-		if (avg_nr_run <= (nr_threshold << (FSHIFT - nr_fshift)))
+		//if (avg_nr_run <= (nr_threshold << (FSHIFT - nr_fshift)))
 			break;
 	}
 	nr_run_last = nr_run;
@@ -224,7 +225,7 @@ static void update_per_cpu_stat(void)
 
 	for_each_online_cpu(cpu) {
 		l_ip_info = &per_cpu(ip_info, cpu);
-		l_ip_info->cpu_nr_running = avg_cpu_nr_running(cpu);
+		//l_ip_info->cpu_nr_running = avg_cpu_nr_running(cpu);
 #ifdef DEBUG_INTELLI_PLUG
 		pr_info("cpu %u nr_running => %lu\n", cpu,
 			l_ip_info->cpu_nr_running);
